3cf42c2fcb6382fc7134a71c63646952fbec069e
Krishna kumara.H <krishnakumarah714@gmail.com>
2026-03-17 21:48:12 +0530

    Add files via upload

---
+++ Logic & Flow/Creating_Dictionary.py
@@ -0,0 +1,8 @@
+def create_student_dict(name, age, major):
+    # Write code here
+    student_dict={
+        "name":name,
+        "age":age,
+        "major":major
+    }
+    return student_dict
