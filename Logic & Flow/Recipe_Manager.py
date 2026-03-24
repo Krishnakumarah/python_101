2441ca5a1602369a75a941a59746260bb88c1ee0
Krishna kumara.H <krishnakumarah714@gmail.com>
2026-03-24 21:37:27 +0530

    Add files via upload

---
+++ Logic & Flow/Recipe_Manager.py
@@ -0,0 +1,8 @@
+recip_book={
+    "Pancakes": ["flour", "milk", "eggs", "sugar"],
+    "Salad": ["lettuce", "tomato", "cucumber", "olive oil"]
+}
+print(recip_book["Pancakes"])
+recip_book["Smoothie"]=["banana", "milk", "honey"]
+recip_book["Smoothie"].append("blueberries")
+print(recip_book)
