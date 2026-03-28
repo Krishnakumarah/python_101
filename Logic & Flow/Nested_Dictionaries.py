9f06c7a1844baead2a45a02333d4890679583a3c
Krishna kumara.H <krishnakumarah714@gmail.com>
2026-03-28 21:27:12 +0530

    Add files via upload

---
+++ Logic & Flow/Nested_Dictionaries.py
@@ -0,0 +1,10 @@
+students = {
+    "Alice": {"age": 20, "grade": "A"},
+    "Bob": {"age": 22, "grade": "B"}
+}
+
+print(students["Alice"]["age"]) 
+
+
+students["Alice"]["major"] = "Math"
+print(students["Alice"])
