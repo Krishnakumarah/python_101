910451659a82ce41120e6f876ddbfe4420b6042f
Krishna kumara.H <krishnakumarah714@gmail.com>
2026-03-29 23:18:50 +0530

    Add files via upload

---
+++ Logic & Flow/Checking_for_Keys.py
@@ -0,0 +1,7 @@
+employees = {"Alice": "HR", "Bob": "Engineering", "Diana": "Marketing"}
+
+# Write code here
+if 'Alice' in employees:
+    print("Alice is in the company.")
+if 'john' not in employees:
+    print("John is not in the company.")
