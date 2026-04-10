a7eb7b5ba0a5c10702077782854b2a56b6cb7822
Krishna kumara.H <krishnakumarah714@gmail.com>
2026-04-10 15:05:41 +0530

    completedcontactbook

--- Logic & Flow/Contact_book_application.py
+++ Logic & Flow/Contact_book_application.py
@@ -8,6 +8,7 @@ def display_menu():
     print("6. Exit")
 
 
+
 def add_contact(contact_book):
     name = input()
     if name in contact_book:
@@ -18,3 +19,77 @@ def add_contact(contact_book):
     address = input()
     contact_book[name] = {"phone": phone, "email": email, "address": address}
     print("Contact added successfully!")
+def view_contact(contact_book):
+    name = input()
+    if name in contact_book:
+        contact = contact_book[name]
+        print(f"Name: {name}")
+        print(f"Phone: {contact['phone']}")
+        print(f"Email: {contact['email']}")
+        print(f"Address: {contact['address']}")
+    else:
+        print("Contact not found!")
+    
+def edit_contact(contact_book):
+    name = input()
+    if name in contact_book:
+        phone = input()
+        email = input()
+        address = input()
+
+        if phone == '':
+            phone = contact_book[name]['phone']
+        if email == '':
+            email = contact_book[name]['email']
+        if address == '':
+            address = contact_book[name]['address']
+
+        contact_book[name] = {"phone": phone, "email": email, "address": address}
+        print("Contact updated successfully!")
+    else:
+        print("Contact not found!")
+
+def delete_contact(contact_book):
+    name=input()
+    if name in contact_book:
+        contact_book.pop(name)
+        print("Contact deleted successfully!")
+    else:
+        print("Contact not found!")
+def list_all_contacts(contact_book):
+    if not contact_book:
+        print("No contacts available.")
+    else:
+        for name ,details in contact_book.items():
+            phone={details['phone']}
+            email={details['email']}
+            address={details['address']}
+            print(f"Name: {name}")
+            print(f"Phone: {details['phone']}")
+            print(f"Email: {details['email']}")
+            print(f"Address: {details['address']}")
+            print()
+
+contact_book = {}
+
+while True:
+    display_menu()
+    choice = input().strip() # .strip() handles spaces and enters
+
+    if not choice: 
+        continue # This stops the "Invalid choice" from appearing on empty input
+
+    if choice == "1":
+        add_contact(contact_book)
+    elif choice == "2":
+        view_contact(contact_book)
+    elif choice == "3":
+        edit_contact(contact_book)
+    elif choice == "4":
+        delete_contact(contact_book)
+    elif choice == "5":
+        list_all_contacts(contact_book)
+    elif choice == "6":
+        break
+    else:
+        print("Invalid choice. Please try again.")
