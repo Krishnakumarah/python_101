d68dab8f1aabf6060a31c189eb6fc54ba34a649f
Krishna kumara.H <krishnakumarah714@gmail.com>
2026-04-26 22:02:07 +0530

    sets

---
+++ Logic & Flow/Remove_Duplicates.py
@@ -0,0 +1,5 @@
+def remove_duplicates(numbers):
+    # Write code here
+    my_set=set(numbers)
+    lis=list(my_set)
+    print(lis)
---
+++ Logic & Flow/Set_Methods.py
@@ -0,0 +1,8 @@
+numbers={10, 20, 30, 40, 50}
+numbers.discard(35)
+print(numbers)
+popped_element=numbers.pop()
+print(popped_element)
+numbers.clear()
+print(numbers)
+
