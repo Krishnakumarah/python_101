2be8720c972913f0a77b5efe77511434ce2fc106
Krishna kumara.H <krishnakumarah714@gmail.com>
2026-05-10 20:59:11 +0530

    filter_and_square_set

--- Fundamentals/Iterating_Over_Sets.py
+++ Fundamentals/Iterating_Over_Sets.py
@@ -1,3 +1,7 @@
 def iterate_and_filter_set(input_set):
     # Write code here
     return{x for x in input_set if x <=10}
+
+def filter_and_square_set(input_set):
+    # Write code here
+    return (x**2  for x in  input_set if x % 2 !=0)
