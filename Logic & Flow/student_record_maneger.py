1a804938aedc5081694c22c4508a5a9edd6e1707
Krishna kumara.H <krishnakumarah714@gmail.com>
2026-06-17 20:56:39 +0530

    List by Course

--- Logic & Flow/student_record_maneger.py
+++ Logic & Flow/student_record_maneger.py
@@ -31,14 +31,29 @@ def calculate_average_grade(name):
     if not grade_set:
         return 0.0
     return float(sum(grade_set)/len(grade_set))
+def list_student_by_course(name,course):
+    if course in courses:
+        return list_students_by_course
+
+def list_students_by_course(course):
+    enrolled_students = []
+    
+    
+    for name, info in student_records.items():
+        
+        if course in info["courses"]:
+            enrolled_students.append(name)
+            
+    return enrolled_students
+ 
+    
+        
 
 
 add_student("Alice", 20, ["Math", "Physics"])
-add_student("Bob", 22, ["Biology", "Chemistry"])
-add_grade("Alice", 90)
-add_grade("Alice", 85)
-add_grade("Bob", 75)
-print(calculate_average_grade("Alice"))  # Should return 87.5
-print(calculate_average_grade("Bob"))  # Should return 75.0
-print(calculate_average_grade("Charlie"))  # Non-existent student, should print message and return None
-print(calculate_average_grade("Alice"))  # Should return 87.5 again
+add_student("Bob", 22, ["Math", "Biology"])
+add_student("Diana", 23, ["Chemistry", "Physics"])
+print(list_students_by_course("Math"))  # Should return ["Alice", "Bob"]
+print(list_students_by_course("Physics"))  # Should return ["Alice", "Diana"]
+print(list_students_by_course("Biology"))  # Should return ["Bob"]
+print(list_students_by_course("History"))  # Should return an empty list
