bee2bc7434389532e93435f24b375043c116d423
Krishna kumara.H <krishnakumarah714@gmail.com>
2026-06-20 20:19:03 +0530

    Sorting Data Efficiently

---
+++ Logic & Flow/Mini and Max.py
@@ -0,0 +1,12 @@
+numbers = [42, 17, 23, 56, 9, 34]
+words = ["kiwi", "apple", "banana", "cherry", "date"]
+
+# Write code here
+Smallest_number=min(numbers)
+Largest_number=max(numbers)
+Smallest_word=min(words)
+Largest_word=max(words)
+print(f"Smallest number: {Smallest_number:}")
+print(f"Largest number: {Largest_number}")
+print(f"Smallest word: {Smallest_word}")
+print(f"Largest word: {Largest_word}")
---
+++ Logic & Flow/Sorting_Data_Efficiently.py
@@ -0,0 +1,17 @@
+# Starter inputs
+numbers = [5, 3, 8, 1, 2]
+words = ["elephant", "cat", "dolphin", "bee"]
+
+
+
+# Replace 'pass' with your code for each task
+ascending_numbers = sorted(numbers)
+descending_numbers = sorted(numbers,reverse=True)
+alphabetical_words = sorted(words)
+length_sorted_words = sorted(words,key=len)
+
+# Print the results
+print("Ascending:", ascending_numbers)
+print("Descending:", descending_numbers)
+print("Alphabetical:", alphabetical_words)
+print("By Length:", length_sorted_words)
